aa3404cd6b58b5c52b21844dc59b298b77975a01
Stephen Quan <squan@esri.com>
2020-04-15 15:37:25 +1000

    Tidied up homepage

--- python-website.py
+++ python-website.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, json
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 @app.route("/")
 def website_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('website', 'index.html')
 
-@app.route('/website/<path:path>')
+@app.route('/<path:path>')
 def website_path(path):
     return send_from_directory('website', path)
 
